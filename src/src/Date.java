2082af2e2a24af8dac38a878da41ee4adfbdab64
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-04-08 15:33:58 -0700

    meow

--- src/src/Date.java
+++ src/src/Date.java
@@ -10,16 +10,29 @@ public class Date {
         if(month > 0 && month <= 12){
             this.month = month;
         } else{
-            System.out.println("Error, invalid month, reenter month: ");
             this.month = newMonth();
         }
         if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            if(day > 0 && day <= 31){
+                this.day = day;
+            }else{
+                this.day = newDay();
+            }
+        }else if(month == 4 || month == 6 || month == 9 || month == 11){
+            if(day > 0 && day <= 30){
+                this.day = day;
+            }else{
+                this.day = newDay();
+            }
+        } else{
 
         }
+
     }
 
     public static int newMonth(){
         Scanner keyboard = new Scanner(System.in);
+        System.out.println("Error, invalid month, reenter month: ");
         int input = keyboard.nextInt();
         if(input > 0 && input <= 12){
              return input;
@@ -27,6 +40,11 @@ public class Date {
             System.out.println("Error, invalid month, reenter month: ");
             return newMonth();
         }
+    }
+    public static int newDay(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("");
+
     }
 
 }
