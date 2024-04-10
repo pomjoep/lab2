033692afb2a1bda057da24b26e3492ba6164e2dd
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-04-10 00:52:18 -0700

    point done

--- src/src/Point2D.java
+++ src/src/Point2D.java
@@ -4,6 +4,22 @@ public class Point2D {
     //private means they cannot be changed or accessed directly,
     //however within the class they can be changed
     //they also do need an object made to exist
+    public static void main(String[] args) {
+        Point2D a = new Point2D();
+        a.setX(5);
+        a.setY(2);
+        System.out.println("Point at (" + a.getX() + ", " + a.getY() + ")");
+        a.translate(-1,-1);
+        System.out.println("Point at (" + a.getX() + ", " + a.getY() + ")");
+        a.resetToOrigin();
+        System.out.println("Point at (" + a.getX() + ", " + a.getY() + ")");
+        Point2D b = new Point2D();
+        Point2D c = new Point2D();
+        System.out.println(b.toString());
+        System.out.println(c); // Question: Why don’t you need c.toString() here?
+        // print calls the toString function of the data, which we have alr made
+        System.out.println("Are b and c equal: " + b.equals(c));
+    }
 
     public void resetToOrigin(){
         x = 0;
